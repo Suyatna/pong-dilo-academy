8bc541833a16d87fbf9cab5329ec23e1ad328910
Suyatna <zuyatna@gmail.com>
2020-08-25 22:44:10 +0700

    implement pong - ball

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BallControl : MonoBehaviour
+{
+    public float xInitialForce;
+    public float yInitialForce;
+    
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        
+        RestartGame();
+    }
+
+    private void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+
+    private void PushBall()
+    {
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        float randomDirection = Random.Range(0, 2);
+
+        if (randomDirection < 1)
+        {
+            _rigidbody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            _rigidbody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+
+    private void RestartGame()
+    {
+        ResetBall();
+        
+        Invoke(nameof(PushBall), 2);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 10f;
+    public float boundaryY = 9f;
+
+    private Rigidbody2D _rigidbody2D;
+    private int _score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = _rigidbody2D.velocity;
+
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            velocity.y = -speed;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
+
+        _rigidbody2D.velocity = velocity;
+
+        if (position.y > boundaryY)
+        {
+            position.y = boundaryY;
+        }
+        else if (position.y < -boundaryY)
+        {
+            position.y = -boundaryY;
+        }
+
+        transform.position = position;
+    }
+
+    public void IncrementScore()
+    {
+        _score++;
+    }
+
+    public void ResetScore()
+    {
+        _score = 0;
+    }
+
+    public int Score => _score;
+}
